de2488bce189bc2648b626e72c8bb3171647f71a
AlinaPerf <158475062+Koniakk@users.noreply.github.com>
2025-07-24 20:15:03 +0300

    First test

--- AviaSales/Program.cs
+++ AviaSales/Program.cs
@@ -1,20 +1,29 @@
+using AviaSales.Service;
+using Microsoft.AspNetCore.DataProtection;
+using AviaSales;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Инициализация контекста данных с строкой подключения к postgreSQL
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbName = Environment.GetEnvironmentVariable("POSTGRES_DB");
+var dbUser = Environment.GetEnvironmentVariable("POSTGRES_USER");
+var dbPassword = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+var connectionString = $"Server = {dbHost};Port=5432;Database={dbName};User Id ={dbUser};Password={dbPassword}";
+builder.Services.AddScoped(provider => new DataContext(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<PassengerService>();
+builder.Services.AddScoped<CountryService>();
+builder.Services.AddScoped<TicketService>();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
@@ -22,4 +31,14 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+//Инициализация БД, проведение миграций
+await InitializeDataSources(app);
+
 app.Run();
+
+async Task InitializeDataSources(WebApplication application)
+{
+    using var scope = application.Services.CreateScope();
+    var dataContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    await dataContext.TryInitializeAsync();
+}
