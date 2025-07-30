19779c600134798cea7d49d30459dc261f66026e
AlinaPerf <158475062+Koniakk@users.noreply.github.com>
2025-07-30 20:25:34 +0300

    Update PlaсeController.cs

--- AviaSales/Controllers/PlaсeController.cs
+++ AviaSales/Controllers/PlaсeController.cs
@@ -5,7 +5,7 @@ using NewsAPI.Data;
 
 namespace AviaSales.Controllers
 {
-    [Route("avia_sales/place")]
+    [Route("avia_sales/country")]
     [ApiController]
     public class PlaceController(PlaceService service) : ControllerBase
     {
