da475ce1db4a79ade33a11c4850d93cc12f06981
AlinaPerf <158475062+Koniakk@users.noreply.github.com>
2025-08-01 18:37:05 +0300

    Update TicketMapper.cs

--- AviaSales/Data/TicketMapper.cs
+++ AviaSales/Data/TicketMapper.cs
@@ -15,6 +15,7 @@ namespace AviaSales.Data
                 Data = topic.Data,  
                 PassengerID = topic.PassengerID,
                 PlaceID = topic.PlaceID,
+                PlaneID = topic.PlaneID,
 
                 UpdatedAt = topic.UpdatedAt,
                 CreatedAt = topic.CreatedAt
@@ -32,6 +33,7 @@ namespace AviaSales.Data
                 Data = topic.Data,
                 PassengerID = topic.PassengerID,
                 PlaceID = topic.PlaceID,    
+                PlaneID = topic.PlaneID,    
                 UpdatedAt = topic.UpdatedAt,
                 CreatedAt = topic.CreatedAt
             };
